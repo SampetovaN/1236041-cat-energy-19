dc5dda129afd04a4409c31fe2b3d1200612b97b4
NikaS <nikasampetova@mail.ru>
2020-05-02 03:51:20 +0300

    Начало автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,65 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include  = require("posthtml-include");
+
+gulp.task("html", function () {
+    return gulp.src("source/*.html")
+        .pipe(posthtml([include()]))
+        .pipe(gulp.dest("source"));
+});
+
+gulp.task("sprite", function () {
+    return gulp.src("source/img/icon-*.svg")
+        .pipe(svgstore({inlineSvg: true}))
+        .pipe(rename("sprite.svg"))
+        .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+    return gulp.src("source/img/**/*.{png,svg}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("images", function () {
+    return gulp.src("source/img/**/*.{png,jpg,svg}")
+        .pipe(imagemin([imagemin.optipng({optimizationLevel: 3}),
+        imagemin.mozjpeg({progressive: true}),
+        imagemin.svgo()]))
+        .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    return gulp.src("source/less/style.less")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(less())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(csso())
+        .pipe(rename("style.min.css"))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("source/css"))
+        .pipe(server.stream());
 });
 
 gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+    server.init({
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+    gulp.watch("source/less/**/*.less", gulp.series("css"));
+    gulp.watch("source/*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
